98edfc23e5cec1edecc8e2dfce605d7bcaec1bbf
kbulatovic <konstantin.bulatovic@snapone.com>
2025-06-05 13:43:31 +0200

    Build stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    base: "/cc-simple-daw/",
     build: {
+        assetsDir: "assets",
         outDir: "docs",
         emptyOutDir: true,
     },
