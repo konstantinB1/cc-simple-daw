9fc1cb7dbf71e85ee18c277b3cc52eb73f63b1fc
kbulatovic <konstantin.bulatovic@snapone.com>
2025-06-05 13:48:37 +0200

    Build stuff

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,10 @@ function main() {
     });
 
     document.body.appendChild(element);
+
+    console.log(
+        "App initialized. If you see this, the app is running correctly.",
+    );
 }
 
 import type { App } from "./App";
